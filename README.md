ab3342b276084ecb4b2cd0c4a3c561a9055856b2
Lacey Wood <yt@nem0.net>
2019-08-08 17:31:28 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SnipAppender
+ Using Snip to read song data from Spotify, append that onto the existing text crawl in Streamlabs OBS
---
+++ snipappender.ps1
@@ -0,0 +1,27 @@
+﻿$file_w = "C:\Users\yt\Documents\Twitch Assets\Snip\Snip.txt"
+$file_r = "C:\Users\yt\Documents\Twitch Assets\d2d about.txt"
+$file_c = "C:\Users\yt\Documents\Twitch Assets\d2d crawl.txt"
+
+Function Register-Watcher {
+    param ($folder)
+    $filter = "*.*" #all files
+    $watcher = New-Object IO.FileSystemWatcher $folder, $filter -Property @{ 
+        IncludeSubdirectories = $false
+        EnableRaisingEvents = $true
+    }
+
+    $changeAction = [scriptblock]::Create('
+        # This is the code which will be executed every time a file change is detected
+        $path = $Event.SourceEventArgs.FullPath
+        $name = $Event.SourceEventArgs.Name
+        $changeType = $Event.SourceEventArgs.ChangeType
+        $timeStamp = $Event.TimeGenerated
+        Write-Host "The file $name was $changeType at $timeStamp"
+        $text = (Get-Content $file_r) + (Get-Content $file_w)
+        Set-Content -Path $file_c -Value $text
+    ')
+
+    Register-ObjectEvent $Watcher "Changed" -Action $changeAction
+}
+
+ Register-Watcher "C:\Users\yt\Documents\Twitch Assets\Snip"
